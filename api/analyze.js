1d24904567d5528d94ae569d0769d7d3eb1a112a
vihvventures-rgb <vihvventures@gmail.com>
2026-05-27 23:22:11 +0530

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,7 +1,30 @@
 export default async function handler(req, res) {
 
-  res.status(200).json({
-    message: "Backend Working"
-  });
+  const response = await fetch(
+    "https://openrouter.ai/api/v1/chat/completions",
+    {
+      method: "POST",
+
+      headers: {
+        "Authorization": "Bearer YOUR_API_KEY",
+        "Content-Type": "application/json"
+      },
+
+      body: JSON.stringify({
+        model: "deepseek/deepseek-chat",
+
+        messages: [
+          {
+            role: "user",
+            content: "Analyze Resume"
+          }
+        ]
+      })
+    }
+  );
+
+  const data = await response.json();
+
+  res.status(200).json(data);
 
 }
